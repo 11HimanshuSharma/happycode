dde946ee3013787ae9f05d686dca0eefaff6b0f5
[1Himanshusharma] <himanshusharma272002@gmail.com>
2022-10-23 19:34:32 +0530

    git commit

--- keyword check using if else.py
+++ keyword check using if else.py
@@ -30,3 +30,27 @@ if keyword.iskeyword(list[2]):
  print("fall : true")
 else:
  print("fall : false")
+ 
+ 
+ #multiple conditions in if else
+ 
+ #using same result multiple conditions
+ # question - find out leap year
+ 
+y = int(input())
+if ((y%400) or (y%4==0) and (y%100!=0)):
+    print("is a leap year")
+else:
+    print("is not leap year")
+    
+    
+#multiple conditons in if else with same conditions and different result
+#quesetion
+
+n = int(input())
+if ((n%2!=0) or ((6<=n<=20) and (n%2==0))):
+    print("Weird")
+elif (((n%2==0) and (2<=n<=5)) or ((n%2==0) and (20<=n))):
+    print("Not Weird")
+else:
+    print("Not Weird")
